124f93b720b3e04b70550ee4f9e95c4fee4b6076
Sohan Yadav <sohan@clouddrove.com>
2019-05-11 14:28:53 +0530

    update

--- example/main.tf
+++ example/main.tf
@@ -1,4 +1,4 @@
-module "acm" {
+module "cdn-key" {
   source = "git::https://github.com/sohanyadav/terraform-aws-cloudfront-public-key.git"
   public-key = "${file("public_key.pem")}"
   name = "public-key"
